5faf87e8430a876b4d77c7c00581365a46fd9c22
Kevin Minyard <84602404+kpminyard@users.noreply.github.com>
2021-07-05 20:51:45 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+const data = [{quote: '"A small stone may only make a small ripple at first, but someday it will be a wave."', author: "Wiegraf Folles"}, {quote: '"The best ways, don\'t always lead to the best results."', author: "Delita Hyral"}, {quote: '"Names don\'t matter. What\'s important is how you live your life."', author: "Ramza Beoulve"}, {quote: '"But we also know that what we see with our eyes alone isn\'t necessarily the truth."', author: "Alazlam Durai"}, {quote: '"Ignorance itself is a crime!"', author: "Miluda Folles"}];
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      quote: "",
+      author: ""
+    };
+  }
+  
+  newQuote() {
+    let number = Math.floor(Math.random() * 5);
+    let newQuote = data[number].quote;
+    let newAuthor = data[number].author;
+    this.setState({quote: newQuote, author: newAuthor});
+  }
+  
+  componentDidMount() {
+    this.newQuote();
+  }
+  
+  render() {
+    return (
+    <div id="quote-box">
+       <p id="text">
+         {this.state.quote}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p id="author">
+          -{this.state.author}
+        </p>
+        <div id="buttons">
+        <button onClick={this.newQuote.bind(this)} id="new-quote">New Quote</button>
+        <a href="twitter.com/intent/tweet" id="tweet-quote"><i className="fa fa-twitter"></i></a>
+        </div>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
